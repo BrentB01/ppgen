f83b5be736bcb014905d075c3e4b7e5e7074fcfb
Brent Barnett <brent.barnett@my.jcu.edu.au>
2023-08-17 12:39:17 +1000

    Complete Assignment1 code

---
+++ Lab4/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 16-Aug-2023 Brent
+ * Lab4
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+typedef unsigned long uint32_t;
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
