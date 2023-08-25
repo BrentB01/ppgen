10429d62680f8636eee83fa0cb8a8ebd151cf8a7
Brent Barnett <brent.barnett@my.jcu.edu.au>
2023-08-25 22:50:25 +1000

    Complete assignment 1

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -8,41 +8,66 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-
 int main(void) {
-  // TODO - Initialise components and variables
-  stdio_init_all();
-  #define RED_LED 11
-  #define BLUE_LED 12
-  #define GREEN_LED 13
-  gpio_init(RED_LED);
-  gpio_init(GREEN_LED);
-  gpio_init(BLUE_LED);
-  gpio_set_dir(RED_LED, GPIO_OUT);
-  gpio_set_dir(GREEN_LED, GPIO_OUT);
-  gpio_set_dir(BLUE_LED, GPIO_OUT);
+    // Initialise components and variables
+    stdio_init_all();
 
-  while (true) {
-    // TODO - Repeated code here
-    char input = getchar_timeout_us(0);
-    if (input != PICO_ERROR_TIMEOUT) {
-    switch (input) {
-                case '1':
-                    gpio_xor_mask(1u << RED_LED);
-                    printf("Toggled red LED\n");
-                    break;
-                case '2':
-                    gpio_xor_mask(1u << GREEN_LED);
-                    printf("Toggled green LED\n");
-                    break;
-                case '3':
-                    gpio_xor_mask(1u << BLUE_LED);
-                    printf("Toggled blue LED\n");
-                    break;
-                default:
-                    break;
+    #define RED_LED 11
+    #define GREEN_LED 12
+    #define BLUE_LED 13
+    #define SW1 2
+    #define SW2 3
+    #define SW3 4
+
+    bool redLedOn = false;
+    bool greenLedOn = false;
+    bool blueLedOn = false;
+
+    // CALCULATE NUMBER OF SWITCHES
+    const uint SWITCH_PINS[] = {SW1, SW2, SW3};
+    const size_t NUM_SWITCHES = sizeof(SWITCH_PINS) / sizeof(SWITCH_PINS[0]);
+
+    // INITIALISE LEDS
+    const uint LED_PINS[] = {RED_LED, GREEN_LED, BLUE_LED};
+    for (size_t i = 0; i < sizeof(LED_PINS) / sizeof(LED_PINS[0]); i++) {
+        gpio_init(LED_PINS[i]);
+        gpio_set_dir(LED_PINS[i], GPIO_OUT);
+    }
+
+    // INITIALISE SWITCHES
+    for (size_t i = 0; i < NUM_SWITCHES; i++) {
+        gpio_init(SWITCH_PINS[i]);
+        gpio_set_dir(SWITCH_PINS[i], GPIO_IN);
+        gpio_pull_up(SWITCH_PINS[i]);
+    }
+
+    while (true) {
+        for (size_t i = 0; i < NUM_SWITCHES; i++) {
+            if (!gpio_get(SWITCH_PINS[i])) { // Button is pressed (LOW)
+                switch (SWITCH_PINS[i]) {
+                    case SW1:
+                        redLedOn = !redLedOn;
+                        printf("Toggled red LED\n");
+                        break;
+                    case SW2:
+                        greenLedOn = !greenLedOn;
+                        printf("Toggled green LED\n"); // MODIFY THIS SO THAT IT READS ALL  CURRENT VALUES (ALL BUTTONS PRESSED SHOULD BE WHITE)
+                        break;
+                    case SW3:
+                        blueLedOn = !blueLedOn;
+                        printf("Toggled blue LED\n");
+                        break;
+                    default:
+                        break;
+                }
+                while (!gpio_get(SWITCH_PINS[i])) {
+                }
             }
         }
+
+        gpio_put(RED_LED, redLedOn);
+        gpio_put(GREEN_LED, greenLedOn);
+        gpio_put(BLUE_LED, blueLedOn);
     }
 
     return 0;
--- Lab4/main.c
+++ Lab4/main.c
@@ -3,14 +3,54 @@
  * rev 1.0 16-Aug-2023 Brent
  * Lab4
  * ***********************************************************/
+#include <stdint.h>
 
-#include "pico/stdlib.h"
-#include <stdbool.h>
-typedef unsigned long uint32_t;
+#define LED_PIN 25
+#define DELAY_ITERATIONS 8000000
+
+#define SIO_BASE 0xd0000000
+#define LED_BASE 0x40014000
+
+#define GPIO_OUT_SET (SIO_BASE + 0x014)
+#define GPIO_OUT_CLR (SIO_BASE + 0x018)
+#define GPIO_OE_SET (SIO_BASE + 0x024)
+
+#define PAD_CONTROL_BASE 0x40014000
+#define PAD_CONTROL_OFFSET 0x68
+
+#define FUNCTION_GPIO_F5 0x05
+
+// Macro to read or write a register's content
+#define CONTENT_OF(addr) (*(volatile uint32_t*)(addr))
+
+// Delay function
+void delay(uint32_t iterations) {
+    for (uint32_t i = 0; i < iterations; i++) {
+        __asm volatile ("nop");
+    }
+}
 
 int main(void) {
-  // TODO - Initialise components and variables
-  while (true) {
-    // TODO - Repeated code here
-  }
+    // Enable GPIO25 pad
+    CONTENT_OF(PAD_CONTROL_BASE + PAD_CONTROL_OFFSET) |= (1 << 6);  // Set bit 6 to enable input (Flag)
+
+    // Select GPIO function for LED_PIN 
+    CONTENT_OF(PAD_CONTROL_BASE + PAD_CONTROL_OFFSET) = (CONTENT_OF(PAD_CONTROL_BASE + PAD_CONTROL_OFFSET) & ~0x80) | FUNCTION_GPIO_F5;
+
+    // Set LED_PIN as output
+    CONTENT_OF(GPIO_OE_SET) &= ~(1 << LED_PIN);
+
+    // TODO: Implement code to enable function (F5) to enable GPIO (check done correctly)
+    // TODO: Check that all definitions are correct
+    while (1) {
+        // Turn on the LED
+        CONTENT_OF(GPIO_OUT_SET) = (1 << LED_PIN);
+        delay(DELAY_ITERATIONS);
+
+        // Turn off the LED
+        CONTENT_OF(GPIO_OUT_CLR) = (1 << LED_PIN);
+        delay(DELAY_ITERATIONS);
+    }
+
+    return 0;
 }
